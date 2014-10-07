c1de36dde52646c9dfe9efd0a74b5a10286dd428
Ben Brostoff <ben.brostoff@gmail.com>
2014-10-07 10:59:02 -0400

    Date range method and tests

--- lib/bentrackergem.rb
+++ lib/bentrackergem.rb
@@ -9,21 +9,34 @@ module BenTrackerGem
   HISTORY = JSON.parse(RestClient.get URL).to_h["days"]
   
   def self.valid_format(date)
-    DateTime.parse(date, "%Y-%m-%d").to_s[0..9]
+    date = DateTime.parse(date, "%Y-%m-%d").to_s[0..9]
+    now = Time.now.to_s[0..9]
+    if date > now || date < "2014-09-30"
+      raise ArgumentError, "Date outside range (data begins 2014-09-30)"
+    end
+    date
+  end
+
+  def self.filter_hash(hash)
+    {:message => hash["message"],
+     :code => hash["code"],
+     :fitness => hash["fitness"]}
   end
 
   def self.day_stats(date)
     date = valid_format(date)
-    if date > valid_format(Time.now.to_s) || date < "2014-09-30"
-      raise ArgumentError, "Date outside range (data begins 2014-09-30)"
-    end
     HISTORY.each do |summary|
       if summary["day_of"] == date
-        return {:message => summary["message"],
-                :code => summary["code"],
-                :fitness => summary["fitness"]}
+        return filter_hash(summary)
       end
     end
   end
+
+  def self.date_range(begin_date, end_date)
+    begin_date, end_date, hold = valid_format(begin_date), valid_format(end_date), []
+    filter = HISTORY.select { |summary| summary["day_of"] >= begin_date && 
+                              summary["day_of"] <= end_date }.
+                              map { |summary| day_stats(summary["day_of"]) }
+  end   
 
 end
--- spec/bentrackergem_spec.rb
+++ spec/bentrackergem_spec.rb
@@ -8,24 +8,44 @@ describe 'BenTrackerGem' do
     expect(response.code).to eq(200)
   end
 
-  it "should raise an argument error for invalid date format" do 
-    expect{ 
-      BenTrackerGem.day_stats("COO") 
-      }.to raise_error(ArgumentError, "invalid date")
+  context "#valid_format" do 
+    
+    it "should raise an argument error for invalid date format" do 
+      expect{ 
+        BenTrackerGem.valid_format("COO") 
+        }.to raise_error(ArgumentError, "invalid date")
+    end
+
+    it "should raise an argument error if date is outside range" do 
+      expect {
+        BenTrackerGem.valid_format("2014-01-01")
+      }.to raise_error(ArgumentError, "Date outside range (data begins 2014-09-30)")
+    end
+
   end
 
-  it "should raise an argument error if date is outside range" do 
-    expect {
-      BenTrackerGem.day_stats("2014-01-01")
-    }.to raise_error(ArgumentError, "Date outside range (data begins 2014-09-30)")
+  context "#day_stats" do 
+
+    it "should return a summary of the day in hash format" do
+      expect(BenTrackerGem.day_stats("2014-10-06")).to eq({
+         :code => 6,
+         :fitness => 14705,
+         :message => "Interview with Avant. Run. Working on app."
+        })
+    end
+
   end
 
-  it "should return a summary of the day in hash format" do
-    expect(BenTrackerGem.day_stats("2014-10-06")).to eq({
-       :code => 6,
-       :fitness => 14705,
-       :message => "Interview with Avant. Run. Working on app."
-      })
+  context "#date_range" do 
+
+    it "should return an array of day summaries given valid begin and end dates" do
+      expect(BenTrackerGem.date_range("2014-10-03", "2014-10-05")).to eq(
+        [BenTrackerGem.day_stats("2014-10-03"),
+         BenTrackerGem.day_stats("2014-10-04"),
+         BenTrackerGem.day_stats("2014-10-05")]
+        )
+    end
+
   end
 
 end
